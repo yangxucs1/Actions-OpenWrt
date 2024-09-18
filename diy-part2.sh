9e9992ce60196d0a55268be47d29d1ecf170e87a
yangxucs1 <35906909+yangxucs1@users.noreply.github.com>
2024-09-19 00:47:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,7 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+#更改TL-WDR7500闪存大小
+sed -i 's/0x7d0000/0xfd0000/g;s/7f0000/ff0000/g' /target/linux/ath79/dts/qca9558_tplink_tl-wdr7500-v3.dts
+sed -i '621s/8/16/' /target/linux/ath79/image/generic-tp-link.mk
