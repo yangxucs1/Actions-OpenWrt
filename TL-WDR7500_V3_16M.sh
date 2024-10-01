3acf6aa00abfc6eb8332b497d809381ccee432df
yangxucs1 <35906909+yangxucs1@users.noreply.github.com>
2024-10-01 21:28:01 +0800

    Update and rename WDR7500_V3_16M.sh to TL-WDR7500_V3_16M.sh

--- TL-WDR7500_V3_16M.sh
+++ TL-WDR7500_V3_16M.sh
@@ -1,4 +1,4 @@
-##### 更改TL-WDR7500修改编译文件16M #####
+##### TL-WDR7500修改编译文件16M #####
 sed -i 's/0x7d0000/0xfd0000/g' target/linux/ath79/dts/qca9558_tplink_tl-wdr7500-v3.dts
 sed -i 's/7f0000/ff0000/g' target/linux/ath79/dts/qca9558_tplink_tl-wdr7500-v3.dts
 sed -i 's/$(Device/tplink-8mlzma)/$(Device/tplink-16mlzma)/g' target/linux/ath79/image/generic-tp-link.mk
